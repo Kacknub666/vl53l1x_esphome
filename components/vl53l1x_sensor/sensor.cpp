84b650864301a6442eaaec4b7855d10b63108b97
Kacknub666 <h.schiedung@gmx.de>
2025-09-18 20:08:38 +0200

    Update sensor.cpp

--- components/vl53l1x_sensor/sensor.cpp
+++ components/vl53l1x_sensor/sensor.cpp
@@ -1,1 +1,17 @@
-#include "sensor.h"
+#include "esphome.h"
+#include "vl53l1x_sensor.h"
+
+class VL53L1XSensor : public PollingComponent, public Sensor {
+ public:
+  VL53L1XSensor() : PollingComponent(1000) {}
+
+  void setup() override {
+    // Initialisiere den Sensor
+  }
+
+  void update() override {
+    // Lese Daten vom Sensor und setze den Sensorwert
+    float value = 0.0f;  // Beispielwert
+    publish_state(value);
+  }
+};
