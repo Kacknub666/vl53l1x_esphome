5b36c96ee5e66dde22028035e4c539de172ffe3b
Kacknub666 <h.schiedung@gmx.de>
2025-09-18 17:37:35 +0200

    Add files via upload

--- components/vl53l1x_sensor/__init__.py
+++ components/vl53l1x_sensor/__init__.py
@@ -1,3 +1,2 @@
 # __init__.py
-# Python-Wrapper, damit ESPHome das Repo als external_component erkennt
-# Die C++-Komponente wird automatisch über includes eingebunden
+# Leer, nur damit ESPHome das Repo als external_component erkennt
